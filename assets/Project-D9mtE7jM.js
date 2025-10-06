78dcc20602aaec82a9ca1109d1df2501bdd8dd2a
Sorn <Sorn>
2025-10-06 14:38:29 +0700

    Updates

---
+++ assets/Project-D9mtE7jM.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Navbar-CmTiiR9Q.js","assets/index-De_Z7KRh.js","assets/index-CmTblPJU.css","assets/Container-J82UPOVA.js"])))=>i.map(i=>d[i]);
+import{j as r,r as t,_ as o}from"./index-De_Z7KRh.js";const a=t.lazy(()=>o(()=>import("./Navbar-CmTiiR9Q.js"),__vite__mapDeps([0,1,2,3]))),e=()=>r.jsx(a,{});export{e as default};
